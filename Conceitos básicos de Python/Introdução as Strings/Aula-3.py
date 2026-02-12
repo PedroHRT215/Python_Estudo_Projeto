8ea114419b86aaad151dc410e4d9475b2cef9e94
Pedro <pedrohrt215@gmail.com>
2026-02-11 21:31:53 -0300

    Aula 3 finalizada, inicio Aula 4

--- Conceitos básicos de Python/Introdução as Strings/Aula-3.py
+++ Conceitos básicos de Python/Introdução as Strings/Aula-3.py
@@ -16,4 +16,29 @@ print(my_str[-1]) #d
 #Se quiser extrair caracteres de um determinado índice para outro,
 #basta separar os start e stop com dois pontos
 my_str_1 = "Hello World"
-print(my_str_1)
+print(my_str_1[1:4]) #ell
+#O índice stop é não inclusivo, nesse caso o [1:4] extraiu apenas os 
+#caracteres do índice de 1, sem incluir o 4, o caracter de índice 4. 
+
+#Também pode omitir os índices start e stop, o Python utiliza 0 por padrão ou
+#o final da string, respectivamente.
+#Isso ocorre quando omitir o índice start:
+print(my_str_1[:7]) #Hello W
+
+#Isso ocorre quando omitir o índice stop:
+print(my_str_1[8:]) #rld
+
+#Caso omitir ambos:
+print(my_str_1[:]) #Hello World
+
+#Além de start e stop, temos o índice step, usado para especificar o incremento
+#entre cada índice no slice. 
+    #string[start:stop:step]
+
+print(my_str_1[0:11:2]) #Hlowrd
+    #No exemplo acima, o fatiamento começa no índice 0, para antes de 11 e 
+    #extrai a cada 2 caractere
+
+#Um truque útil para fazer com o parâmetro step é inverter uma string
+#definindo o step como -1 e omitindo o start e stop
+print(my_str_1[::-1]) # dlrow olleH
---
+++ Conceitos básicos de Python/Introdução as Strings/Aula-4.py
@@ -0,0 +1,13 @@
+#Quais são alguns métodos comuns de string?
+
+#Python fornece diversos métodos de trabalhar com strings.
+# Vejamos alguns deles:
+
+#Definindo uma string
+my_str = "Hello World"
+
+#Upper: retorna uma string com caracteres convertidas para maiúsculas.
+uppercase_my_str = my_str.upper()
+print(uppercase_my_str) #HELLO WORLD
+
+
